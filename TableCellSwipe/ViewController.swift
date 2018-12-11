23a3e0ba613bd5b9bedfe5d589e077ec22b541ab
Rahul Morade <rahulmorade@Rahuls-Mac-mini.local>
2018-12-11 18:01:24 +0530

    Initial Commit

---
+++ TableCellSwipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableCellSwipe
+//
+//  Created by Rahul Morade on 11/12/18.
+//  Copyright © 2018 Rahul Morade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TableCellSwipeTests/TableCellSwipeTests.swift
@@ -0,0 +1,34 @@
+//
+//  TableCellSwipeTests.swift
+//  TableCellSwipeTests
+//
+//  Created by Rahul Morade on 11/12/18.
+//  Copyright © 2018 Rahul Morade. All rights reserved.
+//
+
+import XCTest
+@testable import TableCellSwipe
+
+class TableCellSwipeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
